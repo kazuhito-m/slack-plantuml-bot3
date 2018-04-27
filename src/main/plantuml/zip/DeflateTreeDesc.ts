165397c3d8f1bcd492810d6d7dc0d358d6b3f01b
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-04-28 00:12:56 +0900

    バグフィクス。ライブラリの細心化によってトランスパイルが動かなかった部分を修正。

--- src/main/plantuml/zip/DeflateTreeDesc.ts
+++ src/main/plantuml/zip/DeflateTreeDesc.ts
@@ -2,9 +2,9 @@ import DeflateCT from './DeflateCT';
 
 export default class DeflateTreeDesc {
 
-    public dyn_tree: Array<DeflateCT>;	// the dynamic tree
-    public static_tree: Array<DeflateCT> | null;	// corresponding static tree or NULL
-    public extra_bits: Array<number>;	// extra bits for each code or NULL
+    public dyn_tree: Array<DeflateCT> | null = null;	// the dynamic tree
+    public static_tree: Array<DeflateCT> | null = null;	// corresponding static tree or NULL
+    public extra_bits: Array<number> | null = null;	// extra bits for each code or NULL
     public extra_base = 0;	// base index for extra_bits
     public elems = 0;		// max number of elements in the tree
     public max_length = 0;	// max bit length for the codes

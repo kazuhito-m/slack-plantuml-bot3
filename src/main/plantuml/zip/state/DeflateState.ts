f654c03098d8d9e9b6f6bdce1422b615e9b689a4
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-05-25 06:23:43 +0900

    リファクタリング。メッソッドを移動。

--- src/main/plantuml/zip/state/DeflateState.ts
+++ src/main/plantuml/zip/state/DeflateState.ts
@@ -6,6 +6,9 @@ export default class DeflateState {
     private _strstart: number = 0;
     private _match_start: number;
 
+    private deflate_data: string = "";
+    private deflate_pos: number = 0;
+
     constructor() {
         this._window = new Array(Constant.WINDOW_SIZE);
         this._match_start = 0;
@@ -15,11 +18,24 @@ export default class DeflateState {
         this.constructor();
     }
 
-    public positionCodeOffset = (offset : number):number => {
+    public initDeflateData = (str: string) => {
+        this.deflate_data = str;
+        this.deflate_pos = 0;
+    }
+
+    public read_buff = (offset: number, n: number): number => {
+        const buff = this._window;
+        let i: number;
+        for (i = 0; i < n && this.deflate_pos < this.deflate_data.length; i++)
+            buff[offset + i] = this.deflate_data.charCodeAt(this.deflate_pos++) & 0xff;
+        return i;
+    }
+
+    public positionCodeOffset = (offset: number): number => {
         return this._window[this._strstart + offset] & 0xff;
     }
 
-    public firstPositionCode = ():number => {
+    public firstPositionCode = (): number => {
         return this.positionCodeOffset(0);
     }
 
@@ -27,7 +43,7 @@ export default class DeflateState {
         this._strstart++;
     }
 
-    public movePosition = (length : number) => {
+    public movePosition = (length: number) => {
         this._strstart += length;
     }
 
@@ -37,8 +53,8 @@ export default class DeflateState {
 
     // getter/setter
 
-    public get window() : Array<number> {
-        return  this._window;
+    public get window(): Array<number> {
+        return this._window;
     }
 
     public get strstart() {
@@ -49,7 +65,7 @@ export default class DeflateState {
         return this._match_start;
     }
 
-    public set match_start(value:number) {
+    public set match_start(value: number) {
         this._match_start = value;
     }
 

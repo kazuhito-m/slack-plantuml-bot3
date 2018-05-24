33088198e6303b81f6bab725057db059a5c7cf93
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-05-24 23:06:34 +0900

    リファクタリング。不要なアクセサ削除とフォーマット。

--- src/main/plantuml/zip/Que.ts
+++ src/main/plantuml/zip/Que.ts
@@ -7,26 +7,25 @@ export default class Quw {
     private _qhead: DeflateBuffer | null;
     private _qtail: DeflateBuffer | null;
 
-    private _outcnt: number = 0;
-    private _outoff: number = 0;
+    private _outcnt: number;
+    private _outoff: number;
     private _outbuf: Array<number>;
-
-    private _marked:boolean = false;
 
     constructor() {
         this._free_queue = null;
         this._qhead = null;
         this._qtail = null;
         this._outbuf = new Array(Constant.OUTBUFSIZ);
+        this._outcnt = 0;
+        this._outoff = 0;
     }
 
     public initialize = () => {
         this.constructor();
-        this._marked = true;
     }
 
     public qcopy = (
-        buff: Array<number>, 
+        buff: Array<number>,
         off: number,
         buff_size: number
     ): number => {
@@ -35,64 +34,64 @@ export default class Quw {
         let j: number;
 
         n = 0;
-        while (this.qhead != null && n < buff_size) {
+        while (this._qhead != null && n < buff_size) {
             i = buff_size - n;
-            if (i > this.qhead.len)
-                i = this.qhead.len;
+            if (i > this._qhead.len)
+                i = this._qhead.len;
             //      System.arraycopy(qhead.ptr, qhead.off, buff, off + n, i);
             for (j = 0; j < i; j++)
-                buff[off + n + j] = this.qhead.ptr[this.qhead.off + j];
+                buff[off + n + j] = this._qhead.ptr[this._qhead.off + j];
 
-            this.qhead.off += i;
-            this.qhead.len -= i;
+            this._qhead.off += i;
+            this._qhead.len -= i;
             n += i;
-            if (this.qhead.len == 0) {
-                const p = this.qhead;
-                this.qhead = this.qhead.next;
+            if (this._qhead.len == 0) {
+                const p = this._qhead;
+                this._qhead = this._qhead.next;
                 this.reuse_queue(p);
             }
         }
 
         if (n == buff_size) return n;
 
-        if (this.outoff < this.outcnt) {
+        if (this._outoff < this._outcnt) {
             i = buff_size - n;
-            if (i > this.outcnt - this.outoff)
-                i = this.outcnt - this.outoff;
+            if (i > this._outcnt - this._outoff)
+                i = this._outcnt - this._outoff;
 
 
             // System.arraycopy(outbuf, outoff, buff, off + n, i);
             for (j = 0; j < i; j++)
-                buff[off + n + j] = this.outbuf[this.outoff + j];
-            this.outoff += i;
+                buff[off + n + j] = this._outbuf[this._outoff + j];
+            this._outoff += i;
             n += i;
-            if (this.outcnt == this.outoff) {
-                this.outcnt = this.outoff = 0;
+            if (this._outcnt == this._outoff) {
+                this._outcnt = this._outoff = 0;
             }
         }
         return n;
     }
 
     private reuse_queue = (p: DeflateBuffer) => {
-        p.next = this.free_queue;
-        this.free_queue = p;
+        p.next = this._free_queue;
+        this._free_queue = p;
     }
 
-    public queClear= () => {
-        this.qhead = null;
-        this.outcnt = 0;
-        this.outoff = 0;
+    public queClear = () => {
+        this._qhead = null;
+        this._outcnt = 0;
+        this._outoff = 0;
     }
 
     public nothingQueHead = () => {
-        return this.qhead == null;   
+        return this._qhead == null;
     }
 
     public put_short = (w: number) => {
         w &= 0xffff;
-        if (this.outoff + this.outcnt < Constant.OUTBUFSIZ - 2) {
-            this.outbuf[this.outoff + this.outcnt++] = (w & 0xff);
-            this.outbuf[this.outoff + this.outcnt++] = (w >>> 8);
+        if (this._outoff + this._outcnt < Constant.OUTBUFSIZ - 2) {
+            this._outbuf[this._outoff + this._outcnt++] = (w & 0xff);
+            this._outbuf[this._outoff + this._outcnt++] = (w >>> 8);
         } else {
             this.put_byte(w & 0xff);
             this.put_byte(w >>> 8);
@@ -100,31 +99,31 @@ export default class Quw {
     }
 
     public put_byte = (c: number) => {
-        this.outbuf[this.outoff + this.outcnt++] = c;
-        if (this.outoff + this.outcnt == Constant.OUTBUFSIZ)
+        this._outbuf[this._outoff + this._outcnt++] = c;
+        if (this._outoff + this._outcnt == Constant.OUTBUFSIZ)
             this.qoutbuf();
     }
 
     private qoutbuf = () => {
-        if (this.outcnt != 0) {
+        if (this._outcnt != 0) {
             const q = this.new_queue();
-            if (this.qhead == null)
-                this.qhead = this._qtail = q;
-            else 
+            if (this._qhead == null)
+                this._qhead = this._qtail = q;
+            else
                 this._qtail = q;
-                this._qtail.next = q;
-            q.len = this.outcnt - this.outoff;
+            this._qtail.next = q;
+            q.len = this._outcnt - this._outoff;
             for (let i = 0; i < q.len; i++)
-                q.ptr[i] = this.outbuf[this.outoff + i];
-            this.outcnt = this.outoff = 0;
+                q.ptr[i] = this._outbuf[this._outoff + i];
+            this._outcnt = this._outoff = 0;
         }
     }
 
     private new_queue = (): DeflateBuffer => {
         let p = new DeflateBuffer();
-        if (this.free_queue != null) {
-            p = this.free_queue;
-            this.free_queue = this.free_queue.next;
+        if (this._free_queue != null) {
+            p = this._free_queue;
+            this._free_queue = this._free_queue.next;
         }
         p.next = null;
         p.len = 0;
@@ -133,10 +132,6 @@ export default class Quw {
     }
 
     // getter/setter
-
-    public get markd():boolean {
-        return this._marked;
-    }
 
     public get free_queue(): DeflateBuffer | null {
         return this._free_queue;
@@ -154,28 +149,12 @@ export default class Quw {
         this._qhead = value;
     }
 
-    public get outoff():number {
+    public get outoff(): number {
         return this._outoff;
     }
 
-    public set outoff(value:number) {
+    public set outoff(value: number) {
         this._outoff = value;
-    }
-
-    public get outcnt():number {
-        return this._outcnt;
-    }
-
-    public set outcnt(value:number) {
-        this._outcnt = value;
-    }
-
-    public get outbuf():Array<number> {
-        return this._outbuf;
-    }
-
-    public set outbuf(value:Array<number> )  {
-        this._outbuf = value;
     }
 
 }
